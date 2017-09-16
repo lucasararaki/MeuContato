c478c36bf7e176a92a6af10f84ad7b6b9622748b
lucasararaki <araraki1@hotmail.com>
2017-09-16 00:50:08 -0300

    Ajusta slider de recursos

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var imagens = new Array("img/bem-vindo-1.jpg",
-                        "img/bem-vindo-2.jpg",
-                        "img/bem-vindo-3.jpg",
-                        "img/bem-vindo-4.jpg",
-                        "img/bem-vindo-5.jpg");
+// carrosel do banner
+var imagens = new Array("img/carrosel/carrosel1.jpg",
+                        "img/carrosel/carrosel2.jpg",
+                        "img/carrosel/carrosel3.jpg",
+                        "img/carrosel/carrosel4.jpg",
+                        "img/carrosel/carrosel5.jpg");
 
 var num_img = 4;
 var img_atual =0;
 
 function changeImg(){
    if (img_atual < (num_img - 1) ) {
-     img_atual = img_atual + 1;
+     img_atual++;
    }
    else {
      img_atual = 0;
@@ -19,6 +20,31 @@ function changeImg(){
    var x = setTimeout ("changeImg()", 3000);
 }
 
+// slider dos recursos
+var sliderImg = new Array("img/prints/tela1.png",
+                          "img/prints/tela2.png",
+                          "img/prints/tela3.png",
+                          "img/prints/tela4.png",
+                          "img/prints/tela5.png",
+                          "img/prints/tela6.png",
+                          "img/prints/tela7.png",)
+
+var slider_qtd = 6
+var img_start =0;
+
+function changeImgSlider(){
+   if (img_start < (slider_qtd - 1) ) {
+     img_start++;
+   }
+   else {
+     img_start = 0;
+   }
+
+   document.getElementById("slider").src = sliderImg[img_start];
+   var x = setTimeout ("changeImgSlider()", 3000);
+}
+
 window.onload = function () {
    changeImg();
+   changeImgSlider();
 };
